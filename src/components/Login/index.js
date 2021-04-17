7cb4362baf7336052c4de3452e7aa7ea97b8e6d2
Hansol Yun <hsyoon315@gmail.com>
2021-04-17 21:41:54 +0900

    Add: fetch token to server

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,39 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login";
 
 function Login() {
+  const googleSuccess = async ({ tokenId }) => {
+    try {
+      const res = await fetch("http://localhost:5000/users/login", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${tokenId}`
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const googleFailure = () => {
+    console.log("google sign in unsuccessful");
+  };
+
   return (
-    <div>
-      google login
-    </div>
+    <>
+      <GoogleLogin
+        clientId="286718780152-q76fp5k4tfc9jgt7ltjafeh0rsq9jeq4.apps.googleusercontent.com"
+        render={(renderProps) => (
+          <button type="button" onClick={renderProps.onClick} disabled={renderProps.disabled}>
+            custom google login
+          </button>
+        )}
+        buttonText="Google Login"
+        onSuccess={googleSuccess}
+        onFailure={googleFailure}
+        cookiePolicy="single_host_origin"
+      />
+    </>
   );
 }
 
