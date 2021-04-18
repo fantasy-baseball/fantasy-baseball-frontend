c0d22e8653b72dfa31e914d03de2cb3b1b12828d
Hansol Yun <hsyoon315@gmail.com>
2021-04-19 04:10:53 +0900

    Add: redux store setting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
+import { reducer } from "./reducers";
 import "./index.css";
 import App from "./components/App";
 
+const middleware = [thunk];
+
+if (process.env.NODE_ENV !== "production") {
+  middleware.push(createLogger());
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware));
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
