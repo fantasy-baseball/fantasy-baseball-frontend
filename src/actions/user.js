07c65d600dae1b0ed2b28dc60cc1dfcd8279398c
Hansol Yun <hsyoon315@gmail.com>
2021-04-19 15:36:23 +0900

    Move: saveUser and clearUser to actions/user

--- src/actions/user.js
+++ src/actions/user.js
@@ -21,3 +21,47 @@ export const failLogin = () => ({
 export const deleteUser = () => ({
   type: LOGOUT,
 });
+
+export const saveUser = (tokenId) => async (dispatch) => {
+  dispatch(waitUser());
+
+  try {
+    const res = await fetch(`${process.env.REACT_APP_API_ADDRESS}/users/login`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${tokenId}`,
+      },
+      credentials: "include",
+    });
+    const { result, data } = await res.json();
+
+    if (result === "ok") {
+      dispatch(createUser(data));
+    } else {
+      dispatch(failLogin());
+    }
+  } catch {
+    dispatch(failLogin());
+  }
+};
+
+export const clearUser = () => async (dispatch) => {
+  const token = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("token"))
+    .split("=")[1];
+
+  const res = await fetch(`${process.env.REACT_APP_API_ADDRESS}/users/logout`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    credentials: "include",
+  });
+
+  const { result } = await res.json();
+
+  if (result === "ok") {
+    dispatch(deleteUser());
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,30 +5,22 @@ import {
   LOGIN_FAILURE,
   LOGOUT
 } from "../constants/actionTypes";
-import {
-  createUser,
-  deleteUser,
-  failLogin,
-  waitUser
-} from "../actions/user";
 
 const initialState = {
   user: null,
   isLoading: false,
 };
 
-export const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case LOGIN_PENDING:
       return produce(state, (draft) => {
-        const draftState = draft;
-        draftState.isLoading = true;
+        draft.isLoading = true;
       });
     case LOGIN_SUCCESS:
       return produce(state, (draft) => {
-        const draftState = draft;
-        draftState.isLoading = false;
-        draftState.user = action.payload;
+        draft.isLoading = false;
+        draft.user = action.payload;
       });
     case LOGIN_FAILURE:
       return initialState;
@@ -36,44 +28,6 @@ export const reducer = (state = initialState, action) => {
       return initialState;
     default:
       return state;
-  }
-};
-
-export const saveUser = (tokenId) => async (dispatch) => {
-  dispatch(waitUser());
-  try {
-    const res = await fetch(`${process.env.REACT_APP_API_ADDRESS}/users/login`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${tokenId}`,
-      },
-      credentials: "include",
-    });
-    const { data } = await res.json();
-    dispatch(createUser(data));
-  } catch {
-    dispatch(failLogin());
-  }
-};
-
-export const clearUser = () => async (dispatch) => {
-  const token = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("@token"))
-    .split("=")[1];
-
-  const res = await fetch(`${process.env.REACT_APP_API_ADDRESS}/users/logout`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    credentials: "include",
-  });
-
-  const { result } = await res.json();
-
-  if (result === "ok") {
-    dispatch(deleteUser());
   }
 };
 
