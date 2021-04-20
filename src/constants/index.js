716944a63959875ae4e4d8cec3801bff8824773c
Hansol Yun <hsyoon315@gmail.com>
2021-04-20 20:59:50 +0900

    Add: ENTRY_POSITIONS in constants

---
+++ src/constants/index.js
@@ -0,0 +1,74 @@
+const ENTRY_POSITIONS = [
+  {
+    playerPosition: "Left Fielder",
+    wrapperGridArea: "2 / 2 / span 4 / span 4",
+    cardGridArea: "4 / 4 / span 4 / span 4",
+    rowStart: 3,
+    columnStart: 4,
+  },
+  {
+    playerPosition: "Middle Fielder",
+    wrapperGridArea: "2 / 5 / span 4 / span 4",
+    cardGridArea: "2 / 6 / span 4 / span 4",
+    rowStart: 1,
+    columnStart: 6,
+  },
+  {
+    playerPosition: "Right Fielder",
+    wrapperGridArea: "2 / 9 / span 4 / span 4",
+    cardGridArea: "4 / 4 / span 4 / span 4",
+    rowStart: 3,
+    columnStart: 4,
+  },
+  {
+    playerPosition: "Short Stop",
+    wrapperGridArea: "4 / 4 / span 4 / span 4",
+    cardGridArea: "3 / 5 / span 4 / span 4",
+    rowStart: 2,
+    columnStart: 5,
+  },
+  {
+    playerPosition: "2nd Baseman",
+    wrapperGridArea: "4 / 6 / span 4 / span 4",
+    cardGridArea: "3 / 7 / span 4 / span 4",
+    rowStart: 2,
+    columnStart: 7,
+  },
+  {
+    playerPosition: "3rd Baseman",
+    wrapperGridArea: "6 / 2 / span 4 / span 4",
+    cardGridArea: "3 / 4 / span 4 / span 4",
+    rowStart: 2,
+    columnStart: 4,
+  },
+  {
+    playerPosition: "Pitcher",
+    wrapperGridArea: "6 / 5 / span 4 / span 4",
+    cardGridArea: "3 / 6 / span 4 / span 4",
+    rowStart: 2,
+    columnStart: 6,
+  },
+  {
+    playerPosition: "1st Baseman",
+    wrapperGridArea: "6 / 9 / span 4 / span 4",
+    cardGridArea: "3 / 4 / span 4 / span 4",
+    rowStart: 2,
+    columnStart: 4,
+  },
+  {
+    playerPosition: "CATCHER",
+    wrapperGridArea: "9 / 5 / span 4 / span 4",
+    cardGridArea: "3 / 6 / span 4 / span 4",
+    rowStart: 2,
+    columnStart: 6,
+  },
+  {
+    playerPosition: "DESIGNATED HITTER",
+    wrapperGridArea: "9 / 7 / span 4 / span 4",
+    cardGridArea: "3 / 6 / span 4 / span 4",
+    rowStart: 2,
+    columnStart: 6,
+  },
+];
+
+export default ENTRY_POSITIONS;
