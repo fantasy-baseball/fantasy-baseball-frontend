0dc270408b1e030cff36a9f20d1ca44117ce0026
Hansol Yun <hsyoon315@gmail.com>
2021-04-21 21:48:43 +0900

    Add: check user on refresh

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 const API_URL = process.env.REACT_APP_API_ADDRESS;
 
-export const fetchUser = async (tokenId) => {
+export const fetchUser = async (tokenId, path) => {
   try {
-    const res = await fetch(`${API_URL}/users/login`, {
+    const res = await fetch(`${API_URL}/users/${path}`, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${tokenId}`,
@@ -23,6 +23,7 @@ export const deleteUser = async () => {
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: "include",
     });
     const { result } = await res.json();
 
