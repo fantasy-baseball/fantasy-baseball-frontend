905fe6a7a81555c906b9154c1149c6ec104dbaa1
eluka22 <eluka22@gmail.com>
2021-04-23 01:56:11 +0900

    Add: submit betting in Betting

--- src/components/Betting/index.js
+++ src/components/Betting/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import styled from "styled-components";
-import { fetchPlayers } from "../../api/game";
+import { fetchPlayers, postBetting } from "../../api/game";
 import checkBettingCondition from "../../utils";
+import { showModal } from "../../actions/modal";
 import SearchEntry from "./SearchEntry";
 import Roaster from "../Roaster";
 import BettingInfo from "../BettingInfo";
@@ -82,12 +83,76 @@ function Betting() {
   const [isLoading, setIsLoading] = useState(false);
   const [bettingCondition, setBettingCondition] = useState(checkBettingCondition(new Date()));
 
+  const dispatch = useDispatch();
+
+  const setModalMessage = (
+    contentText,
+    hasLinkButton,
+    path = "/",
+    linkButtonText = "GO TO MAIN"
+  ) => {
+    dispatch(showModal({
+      isVisible: true,
+      title: "로스터 등록",
+      contentText,
+      hasLinkButton,
+      path,
+      linkButtonText,
+    }));
+  };
+
   const handleBettingMoney = (event) => {
     const { value } = event.target;
     setBettingMoney(value);
   };
 
-  const submitBetting = () => {};
+  const submitBetting = async (event) => {
+    event.preventDefault();
+
+    const playersByPosition = Object.entries(roaster);
+    const userRoaster = [];
+
+    for (let i = 0; i < playersByPosition.length; i += 1) {
+      if (playersByPosition[i][1].name === null) {
+        setModalMessage(
+          "10인 로스터를 모두 채워야합니다.",
+          false,
+          "",
+          ""
+        );
+        return;
+      }
+      userRoaster.push(playersByPosition[i][1].kboId);
+    }
+
+    const bettingData = {
+      roaster: userRoaster,
+      bettingMoney,
+    };
+
+    const { result } = await postBetting(bettingData);
+
+    if (result === "duplicate") {
+      setModalMessage(
+        "이미 베팅에 참가하셨습니다.",
+        true
+      );
+      return;
+    }
+
+    if (result === "failure") {
+      setModalMessage(
+        "베팅 참가에 실패하였습니다. 다시 시도해주세요.",
+        true
+      );
+      return;
+    }
+
+    setModalMessage(
+      "베팅 참가에 성공하였습니다.",
+      true
+    );
+  };
 
   useEffect(() => {
     const getPlayers = async () => {
