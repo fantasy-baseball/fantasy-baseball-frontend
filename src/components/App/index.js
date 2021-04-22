0fe0ad8b572b305d98e9946118cf1bcd5975d321
eluka22 <eluka22@gmail.com>
2021-04-23 00:14:32 +0900

    Refactor: move modal to App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Route, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import styled, { ThemeProvider } from "styled-components";
@@ -9,6 +9,7 @@ import Main from "../Main";
 import Login from "../Login";
 import Betting from "../Betting";
 import Header from "../Header";
+import Modal from "../Shared/Modal";
 import { checkUser } from "../../actions/login";
 
 const Wrapper = styled.div`
@@ -22,10 +23,10 @@ const Layout = styled.main`
 `;
 
 function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
   const { user } = useSelector((state) => state.login);
-  const history = useHistory();
+  const modal = useSelector((state) => state.modal);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const checkUserLogin = () => {
     if (document.cookie.indexOf("access_token") === -1) {
@@ -60,8 +61,7 @@ function App() {
                   path="/"
                   render={() => (
                     <Main
-                      isModalVisible={isModalVisible}
-                      setIsModalVisible={setIsModalVisible}
+                      isModalVisible={modal.isVisible}
                     />
                   )}
                 />
@@ -69,12 +69,19 @@ function App() {
               </>
             )
             : (
-              <Route
-                path="/login"
-                render={() => <Login setIsModalVisible={setIsModalVisible} />}
-              />
+              <Route path="/login" component={Login} />
             )}
         </Layout>
+        {modal.isVisible
+          && (
+            <Modal
+              title={modal.title}
+              contentText={modal.contentText}
+              hasLinkButton={modal.hasLinkButton}
+              path={modal.path}
+              linkButtonText={modal.linkButtonText}
+            />
+          )}
       </ThemeProvider>
     </Wrapper>
   );
