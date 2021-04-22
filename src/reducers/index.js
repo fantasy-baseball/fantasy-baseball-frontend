b6ecf56ca79a99f61f036e324d39f7dba356c632
eluka22 <eluka22@gmail.com>
2021-04-23 00:08:08 +0900

    Add: modal reducer in combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import login from "./login";
 import todayGame from "./todayGame";
+import modal from "./modal";
 
 const reducers = combineReducers({
   login,
   todayGame,
+  modal,
 });
 
 export default reducers;
