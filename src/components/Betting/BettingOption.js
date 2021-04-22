98ce6e5fdba1877b85c19199bd21174055c53090
eluka22 <eluka22@gmail.com>
2021-04-23 02:03:15 +0900

    Add: BettingOption

---
+++ src/components/Betting/BettingOption.js
@@ -0,0 +1,62 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import BettingInfo from "../BettingInfo";
+import Slider from "../Shared/Slider";
+import Button from "../Shared/Button";
+
+const Wrapper = styled.article`
+  width: 100%;
+  margin: auto 0 0 0;
+  padding: ${({ theme }) => theme.padding.base};
+  background: ${({ theme }) => theme.color.white};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const InfoList = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+`;
+
+function BettingOption(props) {
+  const {
+    userMoney,
+    bettingMoney,
+    handleBettingMoney,
+    submitBetting
+  } = props;
+
+  return (
+    <Wrapper>
+      <Slider
+        minValue={500}
+        maxValue={userMoney}
+        step={100}
+        value={bettingMoney}
+        handleChange={handleBettingMoney}
+      />
+      <InfoList>
+        <BettingInfo />
+        <Button
+          type="submit"
+          title="BETTING"
+          color="blue"
+          size="small"
+          handleClick={submitBetting}
+        />
+      </InfoList>
+    </Wrapper>
+  );
+}
+
+BettingOption.propTypes = {
+  userMoney: PropTypes.number.isRequired,
+  bettingMoney: PropTypes.number.isRequired,
+  handleBettingMoney: PropTypes.func.isRequired,
+  submitBetting: PropTypes.func.isRequired,
+};
+
+export default BettingOption;
