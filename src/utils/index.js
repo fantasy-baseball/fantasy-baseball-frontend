9001f8741afdd6599e2339b3b6afd4c20c662f68
eluka22 <eluka22@gmail.com>
2021-04-22 21:35:50 +0900

    Refactor: checkBettingFunction in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,27 +1,39 @@
 import { formatDate } from "./date";
-import { GAME_START_TIME } from "../constants";
+import { GAME_START_TIME, BETTING_START_TIME } from "../constants";
 
-const checkBettingStart = (date) => {
-  const day = date.getDay();
+const checkDay = (day) => {
+  if (day === 5) {
+    return "saturday";
+  }
+
+  if (day === 6) {
+    return "sunday";
+  }
+
+  return "weekdays";
+};
+
+const checkBettingCondition = (date) => {
+  const today = checkDay(date.getDay());
   const now = formatDate(date, "kk:mm:ss");
 
-  if ((day === 5 && now > GAME_START_TIME.saturday)) {
+  if (now > GAME_START_TIME[today]) {
     return "close";
   }
 
-  if (day === 6 && now > GAME_START_TIME.sunday) {
-    return "close";
-  }
-
-  if (now > GAME_START_TIME.weekdays) {
-    return "close";
+  if (now < GAME_START_TIME[today] && now > BETTING_START_TIME[today]) {
+    return "open";
   }
 
   if (now > "00:30:00" && now < "04:00:00") {
     return "calculating";
   }
 
-  return "open";
+  if (now > "04:00:00") {
+    return "result";
+  }
+
+  return "close";
 };
 
-export default checkBettingStart;
+export default checkBettingCondition;
