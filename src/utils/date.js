659c573e646ff0f792fdefe1fbebc938d3dbcedd
eluka22 <eluka22@gmail.com>
2021-04-26 10:34:15 +0900

    Add: subDate in utils/date

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,8 +1,15 @@
-import { format, intervalToDuration, parse } from "date-fns";
+import {
+  format,
+  intervalToDuration,
+  parse,
+  subDays,
+} from "date-fns";
 
 export const formatDate = (date, setting) => format(date, setting);
 
 export const parseDate = (date, setting) => parse(date, setting, new Date());
+
+export const subDate = (date, days) => subDays(date, days);
 
 export const countTime = (now, endTime) => {
   const duration = intervalToDuration({
