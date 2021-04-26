51fa54affdf93d3434c6d204d8475d3c80b0cec2
Jeongbeen <pjb6510@gmail.com>
2021-04-27 03:11:48 +0900

    Update: change Loading components path

--- src/components/Betting/LoadingEntry/index.js
+++ src/components/Betting/LoadingEntry/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styled, { useTheme } from "styled-components";
 import { SkeletonTheme } from "react-loading-skeleton";
-import LoadingTable from "./LoadingTable";
-import LoadingBettingOption from "./LoadingBettingOption";
+import LoadingTable from "../../Shared/Loading/LoadingTable";
+import LoadingBettingOption from "../../Shared/Loading/LoadingBettingOption";
 
 const Wrapper = styled.div`
   width: 100%;
