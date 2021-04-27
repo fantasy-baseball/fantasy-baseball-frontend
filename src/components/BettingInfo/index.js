89859645d696eeb2fe06ade09ab0f4c7509fd6ca
eluka22 <eluka22@gmail.com>
2021-04-27 15:55:02 +0900

    Fix: error handling for BettingInfo

--- src/components/BettingInfo/index.js
+++ src/components/BettingInfo/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUsers, faCoins } from "@fortawesome/free-solid-svg-icons";
+import { formatDate } from "../../utils/date";
 import { fetchBettingStatus } from "../../api/game";
 
 const BettingInfo = styled.div`
@@ -22,13 +23,25 @@ const Value = styled.span`
 function SharedBettingInfo() {
   const [bettingUsers, setBettingUsers] = useState([]);
   const [bettingTotalMoney, setBettingTotalMoney] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const getBettingData = async () => {
-      const { users, totalMoney } = await fetchBettingStatus();
+      try {
+        const status = await fetchBettingStatus(formatDate(new Date(), "yyyyMMdd"));
 
-      setBettingUsers(users);
-      setBettingTotalMoney(totalMoney);
+        if (status.result === "none") {
+          setError("현재 베팅 정보를 가져올 수 없습니다.");
+          return;
+        }
+
+        const { users, totalMoney } = status;
+
+        setBettingUsers(users);
+        setBettingTotalMoney(totalMoney);
+      } catch (err) {
+        setError("현재 베팅 정보를 가져올 수 없습니다.");
+      }
     };
 
     getBettingData();
@@ -36,26 +49,30 @@ function SharedBettingInfo() {
 
   return (
     <BettingInfo>
-      <InfoList>
-        <li>
-          <FontAwesomeIcon
-            icon={faUsers}
-            size="lg"
-            color="black"
-          />
-          <Value>
-            {bettingUsers.length}
-          </Value>
-        </li>
-        <li>
-          <FontAwesomeIcon
-            icon={faCoins}
-            size="lg"
-            color="black"
-          />
-          <Value>{bettingTotalMoney}</Value>
-        </li>
-      </InfoList>
+      {error
+        ? <p>{error}</p>
+        : (
+          <InfoList>
+            <li>
+              <FontAwesomeIcon
+                icon={faUsers}
+                size="lg"
+                color="black"
+              />
+              <Value>
+                {bettingUsers.length}
+              </Value>
+            </li>
+            <li>
+              <FontAwesomeIcon
+                icon={faCoins}
+                size="lg"
+                color="black"
+              />
+              <Value>{bettingTotalMoney}</Value>
+            </li>
+          </InfoList>
+        )}
     </BettingInfo>
   );
 }
