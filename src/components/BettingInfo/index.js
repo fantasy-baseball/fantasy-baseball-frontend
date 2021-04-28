8e2642cc7709fc57aff6c636be7cbb3fbe131772
Hansol Yun <hsyoon315@gmail.com>
2021-04-28 09:56:02 +0900

    Refactor: sorting useState and useEffect

--- src/components/BettingInfo/index.js
+++ src/components/BettingInfo/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { faUsers, faCoins } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
